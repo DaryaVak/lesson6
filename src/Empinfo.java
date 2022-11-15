daeffd71c64e63bd5cf7232d5b1e9baaaef09448
limylim <cementary.ley@gmail.com>
2022-11-15 12:38:38 +0300

    Commit

---
+++ src/Empinfo.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+
+public class Empinfo {
+    public ArrayList<Employees> employees;
+
+
+    public class Employees {
+        private String name;
+        private int age;
+    }
+
+    public class Main {
+        public Empinfo empinfo;
+    }
+
+
+}
+
